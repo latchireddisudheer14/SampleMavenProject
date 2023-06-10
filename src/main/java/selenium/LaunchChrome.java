e20b1c29c498dc68773e3afbc5e0dd18b2b8a5fc
Sudheer <latchireddisudheer14@gmail.com>
2023-06-10 21:22:10 +0530

    git mssesiii

--- src/main/java/selenium/LaunchChrome.java
+++ src/main/java/selenium/LaunchChrome.java
@@ -1,14 +1,41 @@
 package selenium;
 
+
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome {
+	
+	 static class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode() {}
+		      ListNode(int val) { this.val = val; }
+		      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+		 }
+	    public static void insert(ListNode l1,int va)
+	    {
+	    	ListNode newNode = new ListNode(va);
+	    	if(l1.next==null)
+	    		l1.next=newNode;
+	    	else {
+	    		ListNode currNode= l1;
+	    		while(currNode.next!=null)
+	    		{
+	    			currNode=currNode.next;
+	    		}
+	    		currNode.next=newNode;
+	    	}
+	    		
+	    	
+	    }
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\000QPC744\\chromedriver_win32\\chromedriver.exe");
+		/*System.setProperty("webdriver.chrome.driver", "C:\\Users\\000QPC744\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://demo.guru99.com/test/newtours/");
 		String expectedTitle = "Welcome: Mercury";
@@ -20,7 +47,22 @@ public class LaunchChrome {
 		}
 		else {
 			System.out.println("failed");
-		}
+		}*/
+		
+		
+	    ListNode ll1= new ListNode(0);
+	    insert(ll1, 2);
+	    insert(ll1,3);
+	    insert(ll1,5);
+	    System.out.println(ll1.next);
+	    
+	    while(ll1!=null)
+	    {
+	    	System.out.println(ll1.val);
+	    	ll1=ll1.next;
+	    	
+	    }       
+		
 
 	}
 
